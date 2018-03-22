abfeae15fe8052ee644b1bc7f8934ef6033c853d
Rajashree Naik <rajashreenaik14@gmail.com>
2018-03-21 21:45:41 -0400

    adding changes

--- assg6-angular-cli/src/app/nav-bar/nav-bar.component.ts
+++ assg6-angular-cli/src/app/nav-bar/nav-bar.component.ts
@@ -7,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  title = 'Name';
+  myName = 'Spurthi Rajashree!';
+  names: Array<any>;
+  constructor() {
+    this.names = [
+      {
+          "Title": "Lorem Ipsum",
+          "Author": "Spurthi Shetty",
+          "ToDo": "Read the entire book"
+          
+      },
+      {
+          "Title": "Into the Water",
+          "Author": "James Walter",
+          "ToDo":  "Completed"
+      },
+      {
+          "Title": "The Girl In the train ",
+          "Author": "Paula Hawkins",
+          "ToDo":  "Next Week"
+      }
+  ];
+  }
+
+  addHero(newHero: string) {
+    if (newHero) {
+      this.names.push("Title:" + newHero.toString);
+    }
+  }
+
+  onClickMe() {
+    
+  }
 
   ngOnInit() {
   }
